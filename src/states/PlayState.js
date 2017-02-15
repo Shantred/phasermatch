ba87bcc2e6e030eadc0839ced496dae017eed0f1
Eric South <shantred@gmail.com>
2017-02-14 19:54:41 -0600

    Fixed an issue where duplicate faces could come up. Also adjusted card scaling and the function to place cards to use the scaling.

--- src/states/PlayState.js
+++ src/states/PlayState.js
@@ -6,7 +6,7 @@ PlayState.prototype = Object.create(Phaser.State.prototype);
 PlayState.prototype.constructor = PlayState;
 PlayState.prototype.create = function() {
 
-    PlayState.prototype.createCards(1);
+    PlayState.prototype.createCards(2);
 };
 
 PlayState.prototype.update = function() {
@@ -39,8 +39,8 @@ PlayState.prototype.createCards = function(level) {
             normalCards: 18,
             trapCards: 0,
             safeCards: 0,
-            xScale: 1,
-            yScale: 1,
+            xScale: 0.75,
+            yScale: 0.75,
         },
         "3" : {
             normalCards: 28,
@@ -83,6 +83,8 @@ PlayState.prototype.createCards = function(level) {
     var posYBase = 150;
     var posX = posXBase;
     var posY = posYBase;
+    var widthBase = 131;
+    var heightBase = 192;
 
     var maxCards = levelData[level]["normalCards"];
 
@@ -97,34 +99,50 @@ PlayState.prototype.createCards = function(level) {
 
     // Choose the faces we'll be using before creating/position cards
     for (var i = 0; i < maxCards / 2; i++) {
+
+        console.log("picking a new face");
+        console.log(availableFacesArray);
+
         // Grab a random face from the remaining choices
         var faceIndex = Math.floor((Math.random() * availableFacesArray.length) + 1);
+
+        console.log("got a few: " + faceIndex);
 
         // Probably not the best way to do it, but we need each face twice
         facesArray.push(availableFacesArray[faceIndex-1]);
         facesArray.push(availableFacesArray[faceIndex-1]);
 
         // Pop this value off of the array
-        availableFacesArray.splice(faceIndex, 1);
+        availableFacesArray.splice(faceIndex-1, 1);
+
+        console.log("faces chosen");
+        console.log(availableFacesArray);
         shuffle(facesArray);
     }
 
     // Now go through all cards and create them with the first face from the shuffled facesArray
     for (var i = 0; i < maxCards; i++) {
-        // Make sure the card fits on the screen width-wise. Cards are 131 pixels wide
-        if (posX + 131 >= 800) {
+        // Make sure the card fits on the screen width-wise.
+        if (posX + (widthBase * levelData[level]["xScale"]) >= 800) {
             posX = posXBase;
-            posY += 192 + 50;
+            posY += (heightBase * levelData[level]["yScale"]) + (25 * levelData[level]["yScale"]);
         }
 
         // Grab a random face from the remaining choices
         var faceIndex = Math.floor((Math.random() * facesArray.length) + 1);
 
-        new NormalCard(game, posX, posY, 'kingcardback', 'kingcard', facesArray.pop());
+        var card = new NormalCard(game, posX, posY, 'kingcardback', 'kingcard', facesArray.pop());
+        
+        // Update scale to fit screen
+        card.scale.setTo(levelData[level]["xScale"], levelData[level]["yScale"]);
+        card.props.originalScale = card.scale.x;
+        //card.props.originalScale = card.scale.x;
+
+        //this.cards.add(card);
 
         // Just for a little extra randomness. This probably isn't necessary nor great for performance
         shuffle(facesArray);
 
-        posX += 131;
+        posX += (widthBase * levelData[level]["xScale"]);
     }
 }
